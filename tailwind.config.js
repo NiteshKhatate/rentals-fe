0d6adb37a3435812711af313ada461424708336f
NiteshK90 <nnkhatate@gmail.com>
2024-11-24 15:07:16 +0530

    Feat: Colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        "primary-text": "#373C3C",
+        "secondary-text": "#878A8A",
+        "success-text": "#14ae5c",
+        "warning-text": "#fcba03",
+        "danger-text": "#F93030",
+        "success-bg": "#47f598",
+        "warning-bg": "#fcd874",
+        "danger-bg": "#f07373",
+        "rentals-white": "#FFFEFC",
+        "rentals-blue": "#0055FF",
+        "rentals-navy": "#102396",
+        "ice-blue": "#F2F7FC",
+        "primary-border": "#AFB1B1",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
